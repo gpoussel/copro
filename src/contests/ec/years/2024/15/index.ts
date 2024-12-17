4195f53ebf443a54f6e251d9dfaf76dc9aff5335
Guillaume Poussel <gpoussel@gmail.com>
2024-12-17 22:53:57 +0100

    Add solution for EC 2024, quest 15

---
+++ src/contests/ec/years/2024/15/index.ts
@@ -0,0 +1,194 @@
+import { EverybodyCodesContest } from "../../../../../types/contest.js"
+import { breadthFirstSearch } from "../../../../../utils/algo.js"
+import utils from "../../../../../utils/index.js"
+import { Vector2 } from "../../../../../utils/vector.js"
+
+// 🎲 Everybody Codes 2024 - Quest 15
+
+function findStartPosition(grid: string[][]) {
+  const startPoint = grid[0].indexOf(".")
+  return new Vector2(startPoint, 0)
+}
+
+function findAllHerbs(grid: string[][]) {
+  const herbs = utils.grid
+    .findPositions(grid, c => !["#", "~", "."].includes(c))
+    .map(Vector2.fromCoordinates)
+    .map(p => utils.grid.at(grid, p))
+  const uniqueHerbs = Array.from(new Set(herbs))
+  return uniqueHerbs
+}
+
+function parseInput(input: string) {
+  const grid = utils.input.readGrid(input)
+  const startPosition = findStartPosition(grid)
+  return {
+    grid,
+    startPosition,
+  }
+}
+
+function solve(input: ReturnType<typeof parseInput>, endPosition: Vector2, herbsToCollect: string[]) {
+  const { grid, startPosition } = input
+
+  interface Node {
+    position: Vector2
+    herbs: string[]
+  }
+
+  const result = breadthFirstSearch<Node>(
+    { position: startPosition, herbs: [] },
+    {
+      adjacents(node) {
+        const { position, herbs } = node
+        const adjacentNodes = []
+        for (const neighbor of position.plusShapeNeighbors()) {
+          const neighborCell = utils.grid.at(grid, neighbor) || "#"
+          if (neighborCell === "#" || neighborCell === "~") continue
+          if (herbsToCollect.includes(neighborCell) && !herbs.includes(neighborCell)) {
+            const adjacentHerbs = [...herbs, neighborCell].sort()
+            adjacentNodes.push({ position: neighbor, herbs: adjacentHerbs })
+          } else {
+            adjacentNodes.push({ position: neighbor, herbs })
+          }
+        }
+        return adjacentNodes
+      },
+      equals(a, b) {
+        return (
+          a.position.equals(b.position) &&
+          a.herbs.length === b.herbs.length &&
+          a.herbs.every((h, i) => h === b.herbs[i])
+        )
+      },
+      key(node) {
+        return `${node.position.str()} ${node.herbs.join("")}`
+      },
+      ends(node: Node) {
+        return node.herbs.length === herbsToCollect.length && node.position.equals(endPosition)
+      },
+    }
+  )
+  return result
+}
+
+function part1(inputString: string) {
+  const input = parseInput(inputString)
+  return solve(input, input.startPosition, findAllHerbs(input.grid))?.distance
+}
+
+function part2(inputString: string) {
+  return part1(inputString)
+}
+
+function part3(inputString: string) {
+  const input = parseInput(inputString)
+
+  // Find the separation column (that splits the entire area in three regions)
+  const height = input.grid.length
+  const splitColumns = []
+  for (let i = 1; i < input.grid[0].length - 2; i++) {
+    let j = 0
+    while (j < input.grid.length && (input.grid[j][i] === "#" || input.grid[j][i + 1] === "#")) {
+      j++
+    }
+    if (j >= height - 2) {
+      splitColumns.push(i)
+    }
+  }
+
+  if (splitColumns.length !== 2) {
+    throw new Error("Could not find the separation columns")
+  }
+  const [middleLeft, middleRight] = splitColumns
+
+  // We know that we will have to go to all regions, so we can just do the three parts separately
+  // First, do the middle region, the end position is the start position for the left position
+  const middleGrid = input.grid.map(row => row.slice(middleLeft, middleRight + 2))
+  const startPointForMiddleGrid = findStartPosition(middleGrid)
+  const leftPointForMiddleGrid = new Vector2(1, middleGrid.length - 2)
+  utils.grid.set(middleGrid, leftPointForMiddleGrid, "<")
+  const rightPointForMiddleGrid = new Vector2(middleGrid[0].length - 2, middleGrid.length - 2)
+  utils.grid.set(middleGrid, rightPointForMiddleGrid, ">")
+
+  const middleResult = solve(
+    { grid: middleGrid, startPosition: startPointForMiddleGrid },
+    startPointForMiddleGrid,
+    findAllHerbs(middleGrid)
+  )
+  if (!middleResult) {
+    throw new Error()
+  }
+  const { distance: middleDistance, path: middlePath } = middleResult
+  const middleHerbs = middlePath[middlePath.length - 1].herbs
+
+  const leftGrid = input.grid.map(row => row.slice(0, middleLeft + 2))
+  const startPointForLeftGrid = new Vector2(leftGrid[0].length - 1, leftGrid.length - 2)
+  const leftResult = solve(
+    { grid: leftGrid, startPosition: startPointForLeftGrid },
+    startPointForLeftGrid,
+    utils.iterate.differenceBy(findAllHerbs(leftGrid), middleHerbs, v => v)
+  )
+  if (!leftResult) {
+    throw new Error()
+  }
+  const { distance: leftDistance, path: leftPath } = leftResult
+  const leftHerbs = leftPath[leftPath.length - 1].herbs
+
+  const herbsCollectedLeftMiddle = utils.iterate.union(middleHerbs, leftHerbs)
+
+  const rightGrid = input.grid.map(row => row.slice(middleRight))
+  const startPointForRightGrid = new Vector2(0, rightGrid.length - 2)
+  const rightResult = solve(
+    { grid: rightGrid, startPosition: startPointForRightGrid },
+    startPointForRightGrid,
+    utils.iterate.differenceBy(findAllHerbs(rightGrid), herbsCollectedLeftMiddle, v => v)
+  )
+  if (!rightResult) {
+    throw new Error()
+  }
+  const { distance: rightDistance } = rightResult
+  return middleDistance + leftDistance + rightDistance
+}
+
+export default {
+  part1: {
+    run: part1,
+    tests: [
+      {
+        input: `
+#####.#####
+#.........#
+#.######.##
+#.........#
+###.#.#####
+#H.......H#
+###########`,
+        expected: 26,
+      },
+    ],
+  },
+  part2: {
+    run: part2,
+    tests: [
+      {
+        input: `
+##########.##########
+#...................#
+#.###.##.###.##.#.#.#
+#..A#.#..~~~....#A#.#
+#.#...#.~~~~~...#.#.#
+#.#.#.#.~~~~~.#.#.#.#
+#...#.#.B~~~B.#.#...#
+#...#....BBB..#....##
+#C............#....C#
+#####################`,
+        expected: 38,
+      },
+    ],
+  },
+  part3: {
+    run: part3,
+    tests: [],
+  },
+} as EverybodyCodesContest
--- src/utils/algo.ts
+++ src/utils/algo.ts
@@ -1,13 +1,7 @@
 import { Vector2 } from "./vector.js"
 import { PriorityQueue } from "./structures/priority-queue.js"
-import { at, Direction, nextDirClockwise, nextDirCounterClockwise } from "./grid.js"
-
-export interface DijkstraNode {
-  position: Vector2
-  dir: Direction
-  score: number
-  path: Vector2[]
-}
+import { at, Direction, inBounds, nextDirClockwise, nextDirCounterClockwise } from "./grid.js"
+import { logEvery } from "./log.js"
 
 export interface DijkstraPath {
   positions: Vector2[]
@@ -16,6 +10,61 @@ export interface DijkstraPath {
 
 function getKey(position: Vector2, dir: Direction) {
   return `${position.str()},${dir}`
+}
+
+export function dijkstraOnGrid<K>(
+  grid: K[][],
+  options: {
+    starts: Vector2[]
+    ends: Vector2[]
+    isMoveValid: (from: K, to: K) => boolean
+    moveCost: number
+  }
+) {
+  interface DijkstraNode {
+    position: Vector2
+    score: number
+    path: Vector2[]
+  }
+  let bestScore = Infinity
+  const priorizer = (node: DijkstraNode) => node.score
+  const priorityQueue = new PriorityQueue<DijkstraNode>(priorizer, PriorityQueue.MIN_HEAP_COMPARATOR)
+  options.starts.forEach(start => priorityQueue.add({ position: start, score: 0, path: [start] }))
+  const visited = new Map<string, number>()
+  const paths: DijkstraPath[] = []
+
+  while (!priorityQueue.isEmpty()) {
+    const { position, score, path } = priorityQueue.poll()
+    const key = position.str()
+
+    if (visited.has(key) && visited.get(key)! < score) continue
+    visited.set(key, score)
+
+    if (options.ends.some(end => position.equals(end))) {
+      if (score < bestScore) {
+        bestScore = score
+      }
+      paths.push({
+        positions: path,
+        score,
+      })
+      continue
+    }
+    for (const dir of ["up", "down", "left", "right"] as Direction[]) {
+      const newPosition = position.move(dir)
+      if (
+        inBounds(grid, newPosition) &&
+        options.isMoveValid(at(grid, position), at(grid, newPosition)) &&
+        !path.some(p => p.equals(newPosition))
+      ) {
+        priorityQueue.add({ position: newPosition, score: score + options.moveCost, path: [...path, newPosition] })
+      }
+    }
+    logEvery(priorityQueue.size(), 5000)
+  }
+
+  const bestPaths = paths.filter(path => path.score === bestScore)
+  return { bestScore, paths, bestPaths }
 }
 
 export function dijkstraWithDirectionsOnGrid<K>(
@@ -30,7 +79,12 @@ export function dijkstraWithDirectionsOnGrid<K>(
     moveCost: number
   }
 ) {
-  let iterations = 0
+  interface DijkstraNode {
+    position: Vector2
+    dir: Direction
+    score: number
+    path: Vector2[]
+  }
   let bestScore = Infinity
   const priorizer = (node: DijkstraNode) => node.score
   const priorityQueue = new PriorityQueue<DijkstraNode>(priorizer, PriorityQueue.MIN_HEAP_COMPARATOR)
@@ -67,9 +121,87 @@ export function dijkstraWithDirectionsOnGrid<K>(
 
     priorityQueue.add({ position, dir: nextDirClockwise(dir), score: score + options.turnCost, path: [...path] })
     priorityQueue.add({ position, dir: nextDirCounterClockwise(dir), score: score + options.turnCost, path: [...path] })
-    iterations++
   }
 
   const bestPaths = paths.filter(path => path.score === bestScore)
   return { bestScore, paths, bestPaths }
+}
+
+export function dijkstraOnGraph<K>(
+  starts: K[],
+  ends: K[],
+  options: {
+    key: (node: K) => string
+    equals: (a: K, b: K) => boolean
+    moves: (node: K, path: K[]) => { to: K; cost: number }[]
+  }
+) {
+  interface DijkstraNode {
+    score: number
+    node: K
+    path: K[]
+  }
+  let bestScore = Infinity
+  const priorizer = (node: DijkstraNode) => node.score
+  const priorityQueue = new PriorityQueue<DijkstraNode>(priorizer, PriorityQueue.MIN_HEAP_COMPARATOR)
+  starts.forEach(start => priorityQueue.add({ score: 0, node: start, path: [start] }))
+  const visited = new Map<string, number>()
+
+  while (!priorityQueue.isEmpty()) {
+    const { node, score, path } = priorityQueue.poll()
+    const key = options.key(node)
+
+    if (visited.has(key) && visited.get(key)! < score) continue
+    visited.set(key, score)
+
+    if (ends.some(end => options.equals(node, end))) {
+      if (score < bestScore) {
+        bestScore = score
+      }
+      continue
+    }
+
+    for (const { to, cost } of options.moves(node, path)) {
+      priorityQueue.add({ node: to, score: score + cost, path: [...path, to] })
+    }
+    logEvery(priorityQueue.size(), 5000)
+  }
+
+  return { bestScore }
+}
+
+export function breadthFirstSearch<K>(
+  start: K,
+  options: {
+    key: (node: K) => string
+    adjacents: (node: K) => K[]
+    equals: (a: K, b: K) => boolean
+    ends: (node: K) => boolean
+  }
+) {
+  interface BreadthFirstSearchNode {
+    node: K
+    distance: number
+    path: K[]
+  }
+  const nodes = new Array<BreadthFirstSearchNode>()
+  nodes.push({ node: start, distance: 0, path: [] })
+
+  const visited = new Set<string>()
+  visited.add(options.key(start))
+
+  while (nodes.length > 0) {
+    const { node, distance, path } = nodes.shift()!
+    if (options.ends(node)) {
+      return { distance, path }
+    }
+    for (const n of options.adjacents(node)) {
+      const neighborKey = options.key(n)
+      if (visited.has(neighborKey)) continue
+      visited.add(neighborKey)
+      nodes.push({ node: n, distance: distance + 1, path: [...path, n] })
+    }
+    logEvery(nodes.length, 5000)
+  }
+  return undefined
 }
