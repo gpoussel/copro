8e027e5f4c2023eb78c7e960fb8596684e47915c
Guillaume POUSSEL <gpoussel@gmail.com>
2024-12-22 15:22:39 +0100

    Add a script to retrieve all inputs for a year

---
+++ loop-aoc.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enableDelayedExpansion
+
+set "year=2017"
+REM Loop through days 1 to 25
+for /l %%d in (1,1,25) do (
+    set "day=%%d"
+    REM Pad day with leading zero
+    set "pday=0!day!"
+    set "pday=!pday:~-2!"
+    echo !year!/!pday!
+
+    CALL pnpm start aoc !year! !day!
+    CALL aoc download --day !day! --year !year! --overwrite --input-only --input-file "E:\dev-oss\copro\src\contests\aoc\years\!year!\!pday!\input"
+)
