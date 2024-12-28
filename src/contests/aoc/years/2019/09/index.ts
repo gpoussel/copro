72b77d88c9de212d4dca3702ec39aa64d99ad771
Guillaume POUSSEL <gpoussel@gmail.com>
2024-12-28 17:48:29 +0100

    Add solution for AoC 2019, day 9

--- src/contests/aoc/years/2019/09/index.ts
+++ src/contests/aoc/years/2019/09/index.ts
@@ -1,7 +1,7 @@
 import { AdventOfCodeContest } from "../../../../../types/contest.js"
 import utils from "../../../../../utils/index.js"
 
-// 🎄 Advent of Code 2019 - Day 7
+// 🎄 Advent of Code 2019 - Day 9
 
 const ADD = "01" // Add
 const MUL = "02" // Multiply
@@ -11,14 +11,17 @@ const JIT = "05" // Jump-if-true
 const JIF = "06" // Jump-if-false
 const LTH = "07" // Less Than
 const EQU = "08" // Equals
+const ARB = "09" // Adjust Relative Base
 const STP = "99" // Stop
 
 const POSITION_MODE = "0"
+const RELATIVE_MODE = "2"
 
 class Computer {
   originalTape: number[]
   tape: number[]
   pointer: number = 0
+  relativeBase: number = 0
   inputs: number[]
   outputs: number[]
   OPS: Record<string, any>
@@ -103,6 +106,12 @@ class Computer {
         fn: (a: number, b: number, c: number) => (this.tape[c] = a === b ? 1 : 0),
         write: true,
       },
+
+      [ARB]: {
+        name: ARB,
+        params: 1,
+        fn: (a: number) => (this.relativeBase += a),
+      },
     }
 
     this.halted = false
@@ -125,7 +134,7 @@ class Computer {
   }
 
   parseOp() {
-    const tempOp = String(this.tape[this.pointer]).padStart(2, "0")
+    const tempOp = String(this.getTapeValue(this.pointer)).padStart(2, "0")
     const op = this.OPS[tempOp.slice(-2)]
     const fullOperationCode = tempOp.padStart(op.params + 2, "0")
 
@@ -141,18 +150,32 @@ class Computer {
     }
   }
 
+  getTapeValue(idx: number) {
+    if (idx < 0) {
+      throw new Error("Invalid tape index")
+    }
+    return this.tape[idx] ?? 0
+  }
+
   runOp({ modes, fn, jumps, write }: { modes: string[]; fn: Function; jumps: boolean; write: boolean }) {
     this.pointer++
     let values = []
     for (let i = 0; i < modes.length; i++) {
       let mode = modes[i]
-      let value = this.tape[this.pointer + i]
+      let value = this.getTapeValue(this.pointer + i)
 
       const canSwitchToPosition = !write || i < modes.length - 1
-      if (canSwitchToPosition && mode === POSITION_MODE) {
-        value = this.tape[value]
+      if (canSwitchToPosition) {
+        if (mode === POSITION_MODE) {
+          value = this.getTapeValue(value)
+        } else if (mode === RELATIVE_MODE) {
+          value = this.getTapeValue(value + this.relativeBase)
+        }
+      } else {
+        if (mode === RELATIVE_MODE) {
+          value += this.relativeBase
+        }
       }
-
       values.push(value)
     }
 
@@ -168,113 +191,50 @@ class Computer {
   }
 }
 
-class Circuit {
-  tape: number[]
-  phaseSettings: number[]
-  computers: Computer[]
-  computerIndex: number
-
-  constructor(tape: number[], phaseSettings: number[], circuitSize = 5) {
-    this.tape = tape
-    this.phaseSettings = phaseSettings
-    this.computers = Array.from({ length: circuitSize }, (_, i) => {
-      const computerPhase = [phaseSettings[i]]
-      if (i === 0) {
-        computerPhase.push(0)
-      }
-
-      return new Computer(tape, computerPhase)
-    })
-
-    this.computerIndex = 0
-  }
-
-  run() {
-    let computer = this.computers[this.computerIndex]
-    let output, lastOutput
-
-    while (!computer.halted) {
-      const newOutput = computer.run()
-      if (computer.halted) {
-        break
-      }
-
-      output = newOutput
-
-      const nextComputer = this.moveToNextComputer()
-      lastOutput = output.shift()!
-      nextComputer.inputs.push(lastOutput)
-
-      computer = nextComputer
-    }
-
-    return lastOutput
-  }
-
-  moveToNextComputer() {
-    this.computerIndex++
-    this.computerIndex %= this.computers.length
-
-    return this.computers[this.computerIndex]
-  }
-}
-
 function parseInput(input: string) {
   return utils.input.firstLine(input).split(",").map(Number)
 }
 
-function findBestPermutations(input: number[], phaseSettings: number[]) {
-  let bestOutput: { output: number; permutation: number[] } | undefined = undefined
-  for (const permutation of utils.iterate.permutations(phaseSettings)) {
-    const circuit = new Circuit(input, permutation)
-    const output = circuit.run()!
-    if (!bestOutput || output > bestOutput.output) {
-      bestOutput = { output, permutation }
-    }
-  }
-  return bestOutput?.output
-}
-
 function part1(inputString: string) {
   const program = parseInput(inputString)
-  return findBestPermutations(program, [0, 1, 2, 3, 4])
+  const computer = new Computer(program, 1)
+  while (!computer.halted) {
+    computer.run()
+  }
+  return computer.outputs.join(",")
 }
 
 function part2(inputString: string) {
   const program = parseInput(inputString)
-  return findBestPermutations(program, [5, 6, 7, 8, 9])
+  const computer = new Computer(program, 2)
+  while (!computer.halted) {
+    computer.run()
+  }
+  return computer.outputs.join(",")
 }
+
+const QUINE = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
 
 export default {
   part1: {
     run: part1,
     tests: [
       {
-        input: "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0",
-        expected: 43210,
+        input: QUINE,
+        expected: QUINE,
       },
       {
-        input: "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
-        expected: 54321,
+        input: "104,1125899906842624,99",
+        expected: "1125899906842624",
       },
       {
-        input: "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
-        expected: 65210,
+        input: "1102,34915192,34915192,7,4,7,99,0",
+        expected: "1219070632396864",
       },
     ],
   },
   part2: {
     run: part2,
-    tests: [
-      {
-        input: "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
-        expected: 139629729,
-      },
-      {
-        input:
-          "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
-        expected: 18216,
-      },
-    ],
+    tests: [],
   },
 } as AdventOfCodeContest
