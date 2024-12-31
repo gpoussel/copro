a6472ff469b360d79de6e850e2d61bdba5eb8739
Guillaume Poussel <gpoussel@gmail.com>
2024-12-31 22:03:21 +0100

    Add solution for PE #3

---
+++ src/contests/euler/problems-00xx/problem-0003.ts
@@ -0,0 +1,16 @@
+import { ProjectEulerProblem } from "../../../types/contest.js"
+import { PRIMES, sieveOfEratosthene } from "../../../utils/math.js"
+
+// 🧮 Project Euler - Problem 3
+
+export function solve() {
+  const num = 600_851_475_143
+  let biggestPrime = 0
+  const primes = sieveOfEratosthene(Math.round(Math.sqrt(num)))
+  for (const prime of primes) {
+    if (num % prime === 0) {
+      biggestPrime = prime
+    }
+  }
+  return biggestPrime
+}
