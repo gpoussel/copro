0c41e7b9fd791c840dd7281e32d21eb08ba9dc28
Guillaume POUSSEL <gpoussel@gmail.com>
2025-12-04 21:10:12 +0100

    Add solution for AoC 2020, day 25

--- src/contests/aoc/years/2020/25/index.ts
+++ src/contests/aoc/years/2020/25/index.ts
@@ -4,34 +4,50 @@ import utils from "../../../../../utils/index.js"
 // 🎄 Advent of Code 2020 - Day 25
 
 function parseInput(input: string) {
-  return
+  return utils.input.lines(input).map(Number)
+}
+
+function findLoopSize(publicKey: number) {
+  let value = 1
+  let loopSize = 0
+  const subjectNumber = 7
+  while (value !== publicKey) {
+    value = (value * subjectNumber) % 20201227
+    loopSize++
+  }
+  return loopSize
 }
 
 function part1(inputString: string) {
-  const input = parseInput(inputString)
-  return
+  const [cardPublicKey, doorPublicKey] = parseInput(inputString)
+  const doorLoopSize = findLoopSize(doorPublicKey)
+  let encryptionKeyFromCard = 1
+  for (let i = 0; i < doorLoopSize; i++) {
+    encryptionKeyFromCard = (encryptionKeyFromCard * cardPublicKey) % 20201227
+  }
+  return encryptionKeyFromCard
 }
 
 function part2(inputString: string) {
-  const input = parseInput(inputString)
-  return
+  return "Merry Christmas!"
 }
 
-const EXAMPLE = ``
+const EXAMPLE = `
+5764801
+17807724`
 
 export default {
   part1: {
     run: part1,
-    tests: [{
-      input: EXAMPLE,
-      expected: undefined,
-    }],
+    tests: [
+      {
+        input: EXAMPLE,
+        expected: 14897079,
+      },
+    ],
   },
   part2: {
     run: part2,
-    tests: [{
-      input: EXAMPLE,
-      expected: undefined,
-    }],
+    tests: [],
   },
 } as AdventOfCodeContest
