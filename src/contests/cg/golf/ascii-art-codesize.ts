280ce04600232fc05ef1a03458e964887259e41d
Guillaume Poussel <gpoussel@gmail.com>
2026-06-01 11:29:29 +0200

    fix: Consolidate loop declaration for ASCII art code size solution

--- src/contests/cg/golf/ascii-art-codesize.ts
+++ src/contests/cg/golf/ascii-art-codesize.ts
@@ -1,2 +1,1 @@
-var R=readline,L=+R(),H=+R(),T=R(),r,i
-for(;H--;)r=R(),console.log(T.replace(/./g,c=>r.substr(((i=parseInt(c,36))>9?i-10:26)*L,L)))
+for(var R=readline,L=+R(),H=+R(),T=R(),r,i;H--;console.log(T.replace(/./g,c=>r.substr(((i=parseInt(c,36))>9?i-10:26)*L,L))))r=R()
